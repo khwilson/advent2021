6dc50f2aed939ea900167296979999c3aa5d54fa
Kevin Wilson <khwilson@gmail.com>
2021-12-06 09:38:51 -0500

    day 6

--- src/advent/solutions/day06.py
+++ src/advent/solutions/day06.py
@@ -1,18 +1,30 @@
-from collections import Counter, defaultdict
-
-import numpy as np
-import pandas as pd
+import copy
 
 from ..solution import Solution
 
 
 class Day06(Solution, day=6):
     def parse(self):
+        data = [0] * 9
         with open(self.input_file, "rt") as infile:
-            pass
+            for fish in infile.read().split(','):
+                data[int(fish)] += 1
+        return data
 
     def part1(self):
-        pass
+        data = copy.copy(self.data)
+        for _ in range(80):
+            to_create = data[0]
+            data[:-1] = data[1:]
+            data[-1] = to_create
+            data[6] += to_create
+        return sum(data)
 
     def part2(self):
-        pass
+        data = copy.copy(self.data)
+        for _ in range(256):
+            to_create = data[0]
+            data[:-1] = data[1:]
+            data[-1] = to_create
+            data[6] += to_create
+        return sum(data)
--- tests/test_day06.py
+++ tests/test_day06.py
@@ -5,9 +5,9 @@ from advent.solutions import day06
 
 def test_part1(fixtures_path: Path):
     solution = day06.Day06(fixtures_path / "test_input06.txt")
-    assert solution.part1() == None
+    assert solution.part1() == 5934
 
 
 def test_part2(fixtures_path: Path):
     solution = day06.Day06(fixtures_path / "test_input06.txt")
-    assert solution.part2() == None
+    assert solution.part2() == 26984457539
