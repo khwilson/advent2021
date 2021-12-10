63834c514e25a66e8bd9d98b2b9739d255e6134c
Kevin Wilson <khwilson@gmail.com>
2021-12-10 00:16:08 -0500

    Ady 10

--- src/advent/solutions/day10.py
+++ src/advent/solutions/day10.py
@@ -1,18 +1,74 @@
 from collections import Counter, defaultdict
+from typing import Optional
 
 import numpy as np
 import pandas as pd
 
 from ..solution import Solution
 
+PAIR = {
+    "(": ")",
+    "{": "}",
+    "[": "]",
+    "<": ">",
+}
+
+for key, val in list(PAIR.items()):
+    PAIR[val] = key
+
+POINTS = {
+    ")": 3,
+    "]": 57,
+    "}": 1197,
+    ">": 25137,
+}
+
+OPOINTS = {
+    ")": 1,
+    "]": 2,
+    "}": 3,
+    ">": 4,
+}
+
+
+def find_illegal_char(line: str) -> Optional[str]:
+    chars = []
+    for char in line:
+        match char:
+            case "(" | "[" | "<" | "{":
+                chars.append(char)
+            case ")" | "]" | ">" | "}":
+                if not chars:
+                    return char, None
+                elif chars[-1] == PAIR[char]:
+                    chars.pop()
+                else:
+                    return char, None
+    return None, chars
+
 
 class Day10(Solution, day=10):
     def parse(self):
         with open(self.input_file, "rt") as infile:
-            pass
+            return [sline for line in infile if (sline := line.strip())]
 
     def part1(self):
-        pass
+        total = 0
+        for line in self.data:
+            char, _ = find_illegal_char(line)
+            if char:
+                total += POINTS[char]
+        return total
 
     def part2(self):
-        pass
+        totals = []
+        for line in self.data:
+            _, chars = find_illegal_char(line)
+            if chars:
+                this_total = 0
+                for char in reversed(chars):
+                    this_total *= 5
+                    this_total += OPOINTS[PAIR[char]]
+                totals.append(this_total)
+        totals.sort()
+        return totals[len(totals) // 2]
--- tests/test_day10.py
+++ tests/test_day10.py
@@ -5,9 +5,9 @@ from advent.solutions import day10
 
 def test_part1(fixtures_path: Path):
     solution = day10.Day10(fixtures_path / "test_input10.txt")
-    assert solution.part1() == None
+    assert solution.part1() == 26397
 
 
 def test_part2(fixtures_path: Path):
     solution = day10.Day10(fixtures_path / "test_input10.txt")
-    assert solution.part2() == None
+    assert solution.part2() == 288957
