9fbf6497b50a542b3f7bdf4338e0c2c3f6763793
Kevin Wilson <khwilson@gmail.com>
2021-12-13 09:51:26 -0500

    Day 13

--- src/advent/solutions/day13.py
+++ src/advent/solutions/day13.py
@@ -1,3 +1,4 @@
+import copy
 from collections import Counter, defaultdict
 
 import numpy as np
@@ -8,11 +9,100 @@ from ..solution import Solution
 
 class Day13(Solution, day=13):
     def parse(self):
+        grid = []
+        folds = []
+        parsing_grid = True
         with open(self.input_file, "rt") as infile:
-            pass
+            for line in infile:
+                line = line.strip()
+                if not line:
+                    parsing_grid = False
+                    continue
+                if parsing_grid:
+                    left, right = map(int, line.split(","))
+                    grid.append((left, right))
+                else:
+                    _, _, instruction = line.split(" ")
+                    axis, val = instruction.split("=")
+                    val = int(val)
+                    folds.append((axis, val))
+
+        return {
+            "grid": set(grid),
+            "folds": folds,
+        }
 
     def part1(self):
-        pass
+        # So the prompt gives an example where all the folds are exactly _halfway_
+        # across the paper, so a 2d gride fold is very easy. However, in the actual
+        # input, the paper has an even, not an odd, width, and so the fold number
+        # cannot be the median. As such, we just do a literal reflection
+        grid = copy.copy(self.data["grid"])
+        folds = self.data["folds"]
+        axis, val = folds[0]
+        new_grid = set()
+
+        if axis == "x":
+            for x, y in grid:
+                if x > val:
+                    # 656 over 655 -> 654 = 655 - (656 - 655)
+                    new_grid.add((val - (x - val), y))
+                elif x == val:
+                    # These seem to just get dropped in the examples
+                    pass
+                else:
+                    # These stay put
+                    new_grid.add((x, y))
+        else:
+            for x, y in grid:
+                if y > val:
+                    # 656 over 655 -> 654 = 655 - (656 - 655)
+                    new_grid.add((x, val - (y - val)))
+                elif x == val:
+                    # These seem to just get dropped in the examples
+                    pass
+                else:
+                    # These stay put
+                    new_grid.add((x, y))
+        grid = new_grid
+        return len(grid)
 
     def part2(self):
-        pass
+        grid = copy.copy(self.data["grid"])
+        folds = self.data["folds"]
+        for axis, val in folds:
+            new_grid = set()
+
+            if axis == "x":
+                for x, y in grid:
+                    if x > val:
+                        # 656 over 655 -> 654 = 655 - (656 - 655)
+                        new_grid.add((val - (x - val), y))
+                    elif x == val:
+                        # These seem to just get dropped in the examples
+                        pass
+                    else:
+                        # These stay put
+                        new_grid.add((x, y))
+            else:
+                for x, y in grid:
+                    if y > val:
+                        # 656 over 655 -> 654 = 655 - (656 - 655)
+                        new_grid.add((x, val - (y - val)))
+                    elif y == val:
+                        # These seem to just get dropped in the examples
+                        pass
+                    else:
+                        # These stay put
+                        new_grid.add((x, y))
+            grid = new_grid
+
+        max_x = max(x for x, _ in grid)
+        max_y = max(y for _, y in grid)
+        np_grid = np.zeros((max_x + 1, max_y + 1), dtype=bool)
+        for x, y in grid:
+            np_grid[x, y] = True
+
+        return "\n" + "\n".join(
+            "".join("#" if val else " " for val in row) for row in np_grid.T
+        )
--- tests/test_day13.py
+++ tests/test_day13.py
@@ -5,9 +5,9 @@ from advent.solutions import day13
 
 def test_part1(fixtures_path: Path):
     solution = day13.Day13(fixtures_path / "test_input13.txt")
-    assert solution.part1() == None
+    assert solution.part1() == 17
 
 
 def test_part2(fixtures_path: Path):
     solution = day13.Day13(fixtures_path / "test_input13.txt")
-    assert solution.part2() == None
+    assert solution.part2() == "\n#####\n#   #\n#   #\n#   #\n#####"
