9f539e143e880a3109db882ff5853802e8c79025
Kevin Wilson <khwilson@gmail.com>
2021-12-15 11:59:26 -0500

    Day 15 utilizing networkx

--- src/advent/solutions/day15.py
+++ src/advent/solutions/day15.py
@@ -1,45 +1,47 @@
-from collections import Counter, defaultdict, deque
-
+import networkx as nx
 import numpy as np
-import pandas as pd
 
 from ..solution import Solution
 from ..utils import around
 
+
 def min_cost(board):
     """
-    Implement as dynamic programming: The minimum cost path from a point is its
-    cost plus the minimum cost of a path from any neighbor.
-
-    Problem is that the two-dimensional nature of the problem makes it difficult to
-    think about the ordering. But if you grow from the bottom-right, it works out,
-    as long as you break whenever the cost of the path seen is higher than the one
-    already there
+    Implement the answer using networkx library. The previous implementation utilized
+    the non-priority queue version of Djikstra, which is V^2 in size, which is rather
+    large. networkx does the priority-queue version which is E + V log V, which is
+    _much_ smaller.
     """
-    cost = np.ones_like(board, dtype=float) * float('inf')
-    init_cost = board[board.shape[0] - 1, board.shape[1] - 1]
-    cost[board.shape[0] - 1, board.shape[1] - 1] = init_cost
-    queue = deque([(board.shape[0] - 1, board.shape[1] - 1, init_cost)])
-    while queue:
-        x, y, this_cost = queue.popleft()
-        for next_x, next_y in around(x, y, board.shape):
-            next_cost = this_cost + board[next_x, next_y]
-            if next_cost < cost[next_x, next_y]:
-                cost[next_x, next_y] = next_cost
-                queue.append((next_x, next_y, next_cost))
-    return int(cost[0, 0] - board[0, 0])
+    g = nx.DiGraph()
+    for x in range(board.shape[0]):
+        for y in range(board.shape[1]):
+            g.add_node((x, y))
+
+    for x in range(board.shape[0]):
+        for y in range(board.shape[1]):
+            for xx, yy in around(x, y, board.shape):
+                g.add_edge((x, y), (xx, yy), cost=board[xx, yy])
+
+    path = nx.shortest_path(
+        g, source=(0, 0), target=(board.shape[0] - 1, board.shape[1] - 1), weight="cost"
+    )
+    return sum(board[x, y] for x, y in path) - board[0, 0]
 
 
 class Day15(Solution, day=15):
     def parse(self):
         with open(self.input_file, "rt") as infile:
-            return np.array([[int(c) for c in sline] for line in infile if (sline := line.strip())])
+            return np.array(
+                [[int(c) for c in sline] for line in infile if (sline := line.strip())]
+            )
 
     def part1(self):
         return min_cost(self.data)
 
     def part2(self):
-        data = np.hstack([self.data, self.data + 1, self.data + 2, self.data + 3, self.data + 4])
+        data = np.hstack(
+            [self.data, self.data + 1, self.data + 2, self.data + 3, self.data + 4]
+        )
         data = np.vstack([data, data + 1, data + 2, data + 3, data + 4])
 
         highs = data // 10
