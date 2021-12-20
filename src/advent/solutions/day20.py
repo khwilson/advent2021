1e1041a98775fd45e7fde1d53e0a81ab36c1b99d
Kevin Wilson <khwilson@gmail.com>
2021-12-20 08:52:08 -0500

    Day 20

--- src/advent/solutions/day20.py
+++ src/advent/solutions/day20.py
@@ -1,18 +1,64 @@
-from collections import Counter, defaultdict
-
-import numpy as np
-import pandas as pd
+import copy
 
 from ..solution import Solution
+
+
+def doit(code, board, num_rounds):
+    for i in range(len(board)):
+        board[i] = (
+            [False] * (num_rounds * 2 + 2) + board[i] + [False] * (num_rounds * 2 + 2)
+        )
+
+    board = (
+        [[0 for _ in range(len(board[0]))] for _ in range(num_rounds * 2 + 2)]
+        + board
+        + [[0 for _ in range(len(board[0]))] for _ in range(num_rounds * 2 + 2)]
+    )
+    for _ in range(num_rounds):
+        new_board = copy.deepcopy(board)
+        for i in range(1, len(board) - 1):
+            for j in range(1, len(board[0]) - 1):
+                x = (
+                    (board[i - 1][j - 1] << 8)
+                    + (board[i - 1][j] << 7)
+                    + (board[i - 1][j + 1] << 6)
+                    + (board[i][j - 1] << 5)
+                    + (board[i][j] << 4)
+                    + (board[i][j + 1] << 3)
+                    + (board[i + 1][j - 1] << 2)
+                    + (board[i + 1][j] << 1)
+                    + (board[i + 1][j + 1])
+                )
+                new_board[i][j] = code[x]
+        board = new_board
+
+        # Shrink the board to deal with boundary issues
+        board = board[1:-1]
+        board = [row[1:-1] for row in board]
+
+    return sum(sum(row) for row in board)
 
 
 class Day20(Solution, day=20):
     def parse(self):
         with open(self.input_file, "rt") as infile:
-            pass
+            code = [c == "#" for c in next(infile).strip()]
+            next(infile)
+            board = [
+                [c == "#" for c in sline] for line in infile if (sline := line.strip())
+            ]
+
+        return {
+            "code": code,
+            "board": board,
+        }
 
     def part1(self):
-        pass
+        code = self.data["code"]
+        board = copy.deepcopy(self.data["board"])
+        return doit(code, board, 2)
 
     def part2(self):
-        pass
+        code = self.data["code"]
+        board = copy.deepcopy(self.data["board"])
+        return doit(code, board, 50)
--- tests/test_day20.py
+++ tests/test_day20.py
@@ -5,9 +5,9 @@ from advent.solutions import day20
 
 def test_part1(fixtures_path: Path):
     solution = day20.Day20(fixtures_path / "test_input20.txt")
-    assert solution.part1() == None
+    assert solution.part1() == 35
 
 
 def test_part2(fixtures_path: Path):
     solution = day20.Day20(fixtures_path / "test_input20.txt")
-    assert solution.part2() == None
+    assert solution.part2() == 3351
